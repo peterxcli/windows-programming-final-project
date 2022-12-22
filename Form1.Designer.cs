31a50bd0eed1a3240cdde57ac4bd6181856543a4
erichung1113 <erichung1113@gmail.com>
2022-12-23 00:23:08 +0800

    create RadioButton

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,11 +28,49 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(24, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(513, 122);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "To Do List";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(640, 18);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 122);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 1012);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "To Do List";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
     }
 
     #endregion
+
+    private Label label1;
+    private Label label2;
 }
--- Form1.cs
+++ Form1.cs
@@ -2,8 +2,18 @@ namespace life_assistant;
 
 public partial class Form1 : Form
 {
+    RadioButton[] things=new RadioButton[10];
     public Form1()
     {
         InitializeComponent();
+        for(int i = 0; i < 5; i++)
+        {
+            things[i]=new RadioButton();
+            things[i].SetBounds(100,150+90*i,200, 100);
+            things[i].Font = new Font("·s²Ó©úÅé", 25F, FontStyle.Regular);
+            things[i].Text = "test";
+            Controls.Add(things[i]);
+        }
     }
+
 }
