ececc6c02c10482342244b60e7a8d4bf91d0f02f
erichung1113 <erichung1113@gmail.com>
2022-12-23 03:21:56 +0800

    version 1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace life_assistant;
+﻿using System.Windows.Forms.VisualStyles;
+
+namespace life_assistant;
 
 partial class Form1
 {
@@ -28,11 +30,60 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+            this.result = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // result
+            // 
+            this.result.Font = new System.Drawing.Font("Microsoft JhengHei UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.result.Location = new System.Drawing.Point(316, 293);
+            this.result.Name = "result";
+            this.result.ReadOnly = true;
+            this.result.Size = new System.Drawing.Size(773, 129);
+            this.result.TabIndex = 0;
+            this.result.Text = "McDonald\'s";
+            this.result.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 19.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(198, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 67);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Result : ";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(570, 644);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(202, 130);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1390, 892);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.result);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
     }
 
     #endregion
+
+    private TextBox result;
+    private Label label1;
+    private Button button1;
 }
--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,33 @@
 namespace life_assistant;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
 
 public partial class Form1 : Form
 {
+    List<String> shop = new List<String>() { "McDonald's","KFC","¤@¤¤¤M«dÄÑ","¤p´U«Î", "¦×¦×±±", "123ª£¶º" };
     public Form1()
     {
         InitializeComponent();
+    }
+    
+
+    private void button1_MouseUp(object sender, MouseEventArgs e)
+    {
+        int cnt = 0;
+        while (cnt++ < 50)
+        {
+            result.Text = shop[cnt % shop.Count()];
+
+            var t = Task.Run(async delegate
+            {
+                await Task.Delay(50);
+                return 42;
+            });
+            t.Wait();
+        }
+        Random R = new Random();
+        result.Text = shop[R.Next(0,shop.Count())];
     }
 }
