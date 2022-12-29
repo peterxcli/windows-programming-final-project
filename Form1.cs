f6b83500be0aa30e0b89175cc8cdb99671c95920
erichung1113 <erichung1113@gmail.com>
2022-12-29 18:18:15 +0800

    add json

--- Form1.cs
+++ Form1.cs
@@ -4,10 +4,21 @@ public partial class Form1 : Form
 {
     RadioButton[] things=new RadioButton[100];
     int ThingsCnt=0;
+    string DataFilePath = "./Data/data.json";
     public Form1()
     {
         InitializeComponent();
-        for(int i = 0; i < 100; i++)
+        
+
+        if (!Directory.Exists(Path.GetDirectoryName(DataFilePath)))
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(DataFilePath));
+        }
+        if (!File.Exists(DataFilePath))
+        {
+            File.Create(DataFilePath);
+        }
+        for (int i = 0; i < 100; i++)
         {
             things[i]=new RadioButton();
             things[i].SetBounds(100,150+80*i,80, 70);
@@ -18,9 +29,22 @@ public partial class Form1 : Form
             things[i].CheckedChanged += radioButton_CheckedChanged;
             Controls.Add(things[i]);
         }
+        StreamReader sr = new StreamReader(DataFilePath);
+        
+        string thing_name = sr.ReadLine();
+        while (thing_name != null)
+        {
+            things[ThingsCnt].Text = thing_name;
+            things[ThingsCnt].Visible = true;
+
+            ThingsCnt++;
+            CntBar.Text = ThingsCnt.ToString();
+            thing_name = sr.ReadLine();
+        }
+        sr.Close();
     }
 
-    private void button1_Click(object sender, EventArgs e)
+    private void insert(object sender, EventArgs e)
     {
         string thing_name = textbar.Text;
         textbar.Text = "";
@@ -35,13 +59,20 @@ public partial class Form1 : Form
 
         ThingsCnt++;
         CntBar.Text = ThingsCnt.ToString();
+
+        StreamWriter sw= new StreamWriter(DataFilePath);
+        for(int i=0;i<ThingsCnt;i++)
+        {
+            sw.WriteLine(things[i].Text);
+        }
+        sw.Close();
     }
 
     private void textbar_KeyDown(object sender, KeyEventArgs e)
     {
         if(e.KeyCode==Keys.Enter)
         {
-            button1_Click(sender, e);
+            insert(sender, e);
             textbar.Focus();
         }
     }
